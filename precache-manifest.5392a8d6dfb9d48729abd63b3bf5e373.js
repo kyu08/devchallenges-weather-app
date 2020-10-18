c7a5e981fab0c73be57408aec77814b6b7c049c5
kyu08 <kyu08@users.noreply.github.com>
2020-10-19 00:05:59 +0900

    Updates

---
+++ precache-manifest.5392a8d6dfb9d48729abd63b3bf5e373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4a0153573c67d1bf42abf363f397bb",
+    "url": "/devchallenges-weather-app/index.html"
+  },
+  {
+    "revision": "500d1fe45176875529e3",
+    "url": "/devchallenges-weather-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0a1c5f46257b00ccba4b",
+    "url": "/devchallenges-weather-app/static/js/2.c9069f4c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/devchallenges-weather-app/static/js/2.c9069f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500d1fe45176875529e3",
+    "url": "/devchallenges-weather-app/static/js/main.c41f33ce.chunk.js"
+  },
+  {
+    "revision": "8f533f95f3ae1ca8677c",
+    "url": "/devchallenges-weather-app/static/js/runtime-main.84680200.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/devchallenges-weather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
